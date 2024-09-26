80111a8f8a55deed5e1bf6597d6857dc5ebe7ab7
bgarz042 <bernardo.garza@marriott.com>
2024-09-26 13:52:03 -0600

    fix

--- Prod/variables.tf
+++ Prod/variables.tf
@@ -1,102 +1,102 @@
 variable "vault_addr" {
-  description = "Marriot vault address"
+  description = "organization vault address"
   type        = string
 }
 
 variable "vault_token" {
-  description = "Marriot vault token"
+  description = "organization vault token"
   type        = string
 }
 
 variable "ldap_organization_url" {
-  description = "Marriot ldap url"
+  description = "organization ldap url"
   type        = string
 }
 
 variable "ldap_organization_user_dn" {
-  description = "Marriot ldap user dn"
+  description = "organization ldap user dn"
   type        = string
 }
 
 variable "ldap_organization_user_filter" {
-  description = "Marriot ldap user filter"
+  description = "organization ldap user filter"
   type        = string
 }
 
 variable "ldap_organization_user_attr" {
-  description = "Marriot ldap user attr"
+  description = "organization ldap user attr"
   type        = string
 }
 
 variable "ldap_organization_group_dn" {
-  description = "Marriot ldap group dn"
+  description = "organization ldap group dn"
   type        = string
 }
 
 variable "ldap_organization_group_attr" {
-  description = "Marriot ldap group attr"
+  description = "organization ldap group attr"
   type        = string
 }
 
 variable "ldap_organization_bind_dn" {
-  description = "Marriot ldap bind dn"
+  description = "organization ldap bind dn"
   type        = string
 }
 
 variable "ldap_organization_password" {
-  description = "Marriot ldap password"
+  description = "organization ldap password"
   default = ""
   type        = string
 }
 
 variable "ldap_organization_vault_admin_group" {
-  description = "Marriot vault admin group"
+  description = "organization vault admin group"
   type        = string
 }
 
 
 variable "ldap_organization_sp_url" {
-  description = "Marriot sp ldap url"
+  description = "organization sp ldap url"
   type        = string
 }
 
 variable "ldap_organization_sp_user_dn" {
-  description = "Marriot sp ldap user dn"
+  description = "organization sp ldap user dn"
   type        = string
 }
 
 variable "ldap_organization_sp_user_filter" {
-  description = "Marriot sp ldap user filter"
+  description = "organization sp ldap user filter"
   type        = string
 }
 
 variable "ldap_organization_sp_user_attr" {
-  description = "Marriot sp ldap user attr"
+  description = "organization sp ldap user attr"
   type        = string
 }
 
 variable "ldap_organization_sp_group_dn" {
-  description = "Marriot sp ldap group dn"
+  description = "organization sp ldap group dn"
   type        = string
 }
 
 variable "ldap_organization_sp_group_attr" {
-  description = "Marriot sp ldap group attr"
+  description = "organization sp ldap group attr"
   type        = string
 }
 
 variable "ldap_organization_group_filter" {
-  description = "Marriot ldap group filter"
+  description = "organization ldap group filter"
   type        = string
 }
 
 variable "ldap_organization_sp_bind_dn" {
-  description = "Marriot sp ldap bind dn"
+  description = "organization sp ldap bind dn"
   type        = string
 }
 
 variable "ldap_organization_sp_password" {
-  description = "Marriot sp ldap password"
+  description = "organization sp ldap password"
   default = ""
   type        = string
 }
@@ -107,37 +107,37 @@ variable "oidc_organization_bound_issuer" {
 }
 
 variable "oidc_organization_default_role" {
-  description = "Marriot oidc default role"
+  description = "organization oidc default role"
   type        = string
 }
 
 variable "oidc_organization_client_id" {
-  description = "Marriot oidc client id"
+  description = "organization oidc client id"
   type        = string
 }
 
 variable "oidc_organization_client_secret" {
-  description = "Marriot oidc client id"
+  description = "organization oidc client id"
   type        = string
 }
 
 variable "oidc_organization_oidc_discovery_url" {
-  description = "Marriot oidc discpvery url"
+  description = "organization oidc discpvery url"
   type        = string
 }
 
 variable "oidc_organization_groups_claim" {
-  description = "Marriot oidc groups claim"
+  description = "organization oidc groups claim"
   type        = string
 }
 
 variable "ldap_organization_sp_group_filter" {
-  description = "Marriot sp ldap group filter"
+  description = "organization sp ldap group filter"
   type        = string
 }
 
 variable "oidc_organization_user_claim" {
-  description = "Marriot oidc user claim"
+  description = "organization oidc user claim"
   type        = string
 }
 
@@ -158,6 +158,6 @@ variable "MI_International_teams_and_services" {
 }
 
 variable "oidc_organization_vault_admin_group" {
-  description = "Marriot vault admin group"
+  description = "organization vault admin group"
   type        = string
 }
